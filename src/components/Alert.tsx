bc38dd61c5af040dbf7025cea946adb1bd9d72ca
Simon Petrac <pfoerdie@gmail.com>
2024-06-03 16:34:31 +0200

    wip

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,30 @@
+'use client'
+
 import { Props, Element, joinNames } from '@util/components'
+import { useEffect, useRef } from 'react'
+// import * as bootstrap from 'bootstrap/js/index.esm'
+import BSAlert from 'bootstrap/js/dist/alert'
 
 export type AlertProps = Props<{
   variant: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+  dismissible?: boolean
 }>
 
 export type AlertElement = Element<AlertProps>
 
-export default function Alert({ variant, children, className }: AlertProps): AlertElement {
-  return <div className={joinNames('alert', `alert-${variant}`, className)} role='alert'>
+export default function Alert({ variant, dismissible, children, className }: AlertProps): AlertElement {
+  const alertRef = useRef(null)
+
+  useEffect(() => {
+    if (!alertRef.current) return
+    const bsAlert = new BSAlert(alertRef.current)
+    return () => bsAlert.dispose()
+  }, [alertRef])
+
+  // TODO wip
+
+  return <div ref={alertRef} className={joinNames('alert', `alert-${variant}`, dismissible ? 'alert-dismissible' : undefined, className)} role='alert'>
     {children}
+    {dismissible && <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>}
   </div>
 }
